3aa88623b9a29ed5470e54eda690633f457d4f1e
Takayuki Sato <sttk.xslet@gmail.com>
2023-04-08 17:15:43 +0900

    chore: modified build.sh (#26)

--- build.sh
+++ build.sh
@@ -1,26 +1,46 @@
 #!/usr/bin/env bash
 
+errcheck() {
+  exitcd=$1
+  if [[ "$exitcd" != "0" ]]; then
+    exit $exitcd
+  fi
+}
+
 clean() {
   go clean --cache
+  errcheck $?
 }
 
 format() {
   go fmt ./...
+  errcheck $?
 }
 
 compile() {
   go vet ./...
+  errcheck $?
   go build ./...
+  errcheck $?
 }
 
 test() {
   go test -v ./...
+  errcheck $?
+}
+
+unit() {
+  go test -v ./... -run $1
+  errcheck $?
 }
 
 cover() {
   mkdir -p coverage
+  errcheck $?
   go test -coverprofile=coverage/cover.out ./...
+  errcheck $?
   go tool cover -html=coverage/cover.out -o coverage/cover.html
+  errcheck $?
 }
 
 bench() {
@@ -29,45 +49,49 @@ bench() {
     dir="."
   fi
   pushd $dir
-  go test -bench . -benchmem -run=^$
+  go test -bench . --benchmem -run=^$
+  errcheck $?
   popd
 }
 
-if [[ $# == 0 ]]; then
+if [[ "$#" == "0" ]]; then
   clean
   format
   compile
   test
   cover
-  exit 0
-fi
 
-for a in "$@"; do
-  case "$a" in
-  clean)
-    clean
-    ;;
-  format)
-    format
-    ;;
-  compile)
-    compile
-    ;;
-  test)
-    test
-    ;;
-  cover)
-    cover
-    ;;
-  bench)
-    bench
-    ;;
-  '')
-    compile
-    ;;
-  *)
-    echo "Bad task: $a"
-    exit 1
-    ;;
-  esac
-done
+elif [[ "$1" == "unit" ]]; then
+  unit $2
+
+else
+  for a in "$@"; do
+    case "$a" in
+    clean)
+      clean
+      ;;
+    format)
+      format
+      ;;
+    compile)
+      compile
+      ;;
+    test)
+      test
+      ;;
+    cover)
+      cover
+      ;;
+    bench)
+      bench
+      ;;
+    '')
+      compile
+      ;;
+    *)
+      echo "Bad task: %a"
+      exit 1
+      ;;
+    esac
+  done
+fi
